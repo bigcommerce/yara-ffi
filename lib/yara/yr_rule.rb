d43fa67d958838c86f68ee13e88464eab7cfde59
Chris Boulton <chris@chrisboulton.com>
2024-11-21 17:28:25 -0800

    align YrRule to YR_RULE struct in yara 4.4

--- lib/yara/yr_rule.rb
+++ lib/yara/yr_rule.rb
@@ -2,6 +2,8 @@ module Yara
   class YrRule < FFI::Struct
     layout \
       :flags, :int,
+      :num_atoms, :int,
+      :required_strings, :int,
       :identifier, :string,
       :tags, :string,
       :metas, :pointer,
